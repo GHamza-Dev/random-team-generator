bde6eaaf1869768a4d671fe3c6c6a3967957959b
ghamza-dev <elgassaihamzam54@gmail.com>
2022-11-27 21:24:32 +0100

    Add app title

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'my-app';
   newMember = '';
   members: string[] = [];
   errorMessage: string = '';
