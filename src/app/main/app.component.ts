329004cbfcae7c046ef9729e7031665bb295e425
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2023-01-02 10:41:55 +0100

    add : remove item

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
@@ -13,20 +13,20 @@ export class AppComponent {
   numberOfTeams: number = 0;
   generatedTeams: string[][] = [];
 
-  onInput(memberInput: string){
+  onInput(memberInput: string) {
     this.newMember = memberInput;
   }
 
-  setNumberOfTeams(nbrOfTeams: string){
+  setNumberOfTeams(nbrOfTeams: string) {
     this.numberOfTeams = parseInt(nbrOfTeams);
   }
 
-  addMember(){
-
+  addMember() {
     this.resetErrorMessage();
+    // console.log('add', this.members);
 
     if (this.newMember === '') {
-      this.errorMessage = "Please enter a member name!";
+      this.errorMessage = 'Please enter a member name!';
       return;
     }
 
@@ -34,15 +34,39 @@ export class AppComponent {
     this.newMember = '';
   }
 
-  generateTeams(){
-
-    this.resetErrorMessage();
-    
-    if(!this.inputIsValid()){
-      return;
+  deleteMember(index: number) {
+    if (index == 0 || index == this.members.length - 1) {
+      this.members.splice(index, index + 1);
+    } else {
+      this.members.splice(index, index - 1);
     }
 
-    let numberOfMembersForEachTeam = Math.floor(this.members.length / this.numberOfTeams);
+    // console.log('before', this.numberOfTeams);
+    if (this.numberOfTeams > this.members.length) {
+      this.numberOfTeams--;
+      // console.log('inside', this.numberOfTeams);
+    }
+    this.generateTeams(false);
+    // console.log('after', this.numberOfTeams);
+
+    // console.log(this.members);
+    // console.log(index);
+  }
+
+  generateTeams(check: boolean) {
+    this.resetErrorMessage();
+
+    if (check) {
+      if (!this.inputIsValid()) {
+        return;
+      }
+    }
+
+    // console.log('inside');
+
+    let numberOfMembersForEachTeam = Math.floor(
+      this.members.length / this.numberOfTeams
+    );
 
     this.resetGeneratedTeams();
 
@@ -50,51 +74,53 @@ export class AppComponent {
     let tmpMembers = [...this.members];
     let rand = 0;
 
+    // not fixed yet
     for (let i = 0; i < this.numberOfTeams; i++) {
-        for (let j = 0; j < numberOfMembersForEachTeam; j++) {
-          rand = this.random(tmpMembers.length)-1;
-          let member = tmpMembers.splice(rand,1)[0];
-          team.push(member);          
-        }
-        this.generatedTeams.push(team);
-        team = [];
+      for (let j = 0; j < numberOfMembersForEachTeam; j++) {
+        rand = this.random(tmpMembers.length) - 1;
+        let member = tmpMembers.splice(rand, 1)[0];
+        team.push(member);
+      }
+      this.generatedTeams.push(team);
+      team = [];
     }
 
-    
-    if(!(this.members.length % this.numberOfTeams === 0)){
-      let randomTeam = this.random(this.numberOfTeams)-1;
-      this.generatedTeams[randomTeam].push(tmpMembers[0]);
+    if (check) {
+      if (!(this.members.length % this.numberOfTeams === 0)) {
+        let randomTeam = this.random(this.numberOfTeams) - 1;
+        this.generatedTeams[randomTeam].push(tmpMembers[0]);
+      }
     }
 
-    console.log(this.generatedTeams);
+    // console.log('hello', this.generatedTeams);
   }
 
-  random(max: number): number{
-    return Math.floor(Math.random()*max+1);
+  random(max: number): number {
+    return Math.floor(Math.random() * max + 1);
   }
 
-  resetErrorMessage(){
+  resetErrorMessage() {
     this.errorMessage = '';
   }
 
-  resetGeneratedTeams(){
+  resetGeneratedTeams() {
     this.generatedTeams = [];
   }
 
-  inputIsValid(): boolean{
-
-    if(this.members.length === 0){
-      this.errorMessage = "Please add members first!";
+  inputIsValid(): boolean {
+    if (this.members.length === 0) {
+      this.errorMessage = 'Please add members first!';
       return false;
     }
 
-    if(!this.numberOfTeams || this.numberOfTeams <= 0){
-      this.errorMessage = "Number of teams can not be less than 1 ;)";
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = 'Number of teams can not be less than 1 ;)';
       return false;
     }
 
-    if(this.numberOfTeams > this.members.length){
-      this.errorMessage = "Number of teams can not be greater than the number of members ;)";
+    if (this.numberOfTeams > this.members.length) {
+      this.errorMessage =
+        'Number of teams can not be greater than the number of members ;)';
       return false;
     }
 
